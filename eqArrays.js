14c79335b88ae2a506dccd170b94efe6215e1cbf
fathel80 <93504424+fathel80@users.noreply.github.com>
2021-11-24 14:57:04 -0500

    Update eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,27 +1,18 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length){
-      return false
-  }
-  for(let i=0; i < array1.length; i++){
-    if  (array1[i] !== array2[i]){
-        return false
-    } 
-  }
-  return true
-}
+const assertEqual = require("./assertEqual");
 
-
-const assertArraysEqual = function(actual, expected) {
-  const isEqual = eqArrays(actual, expected)
-  if (isEqual) {
-    console.log(✅ ✅ ✅   Assertion passed: ${actual} === ${expected});
-  } else {
-    console.log(🛑 🛑 🛑  Assertion failed: ${actual} !== ${expected});
+const eqArrays = function(actual, expected) {
+  let check = true;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      check = false;
+    }
   }
+
+  if (actual.length !== expected.length) {
+    check = false;
+  }
+
+  return check;
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => Passed
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => Failed
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => Passed 
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => Failed
+module.exports = eqArrays;
